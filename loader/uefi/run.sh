716ba9b0b1b67a949f8923e6005362af2ef3309a
Michał 'Griwes' Dominiak <griwes@griwes.info>
2017-01-24 23:58:05 +0100

    -no-kvm master race.

--- loader/uefi/run.sh
+++ loader/uefi/run.sh
@@ -4,5 +4,5 @@ set -e
 
 make
 mcopy -o -i fat.img BOOTX64.EFI ::/EFI/BOOT/BOOTX64.EFI
-qemu-system-x86_64 -enable-kvm -bios ../../deps/ovmf/OVMF.fd -hda fat.img -monitor stdio -parallel file:/dev/stdout -cpu qemu64,+sse3,+sse4.1,+sse4.2,+x2apic -m 2048
+qemu-system-x86_64 -no-kvm -bios ../../deps/ovmf/OVMF.fd -hda fat.img -monitor stdio -parallel file:/dev/stdout -cpu qemu64,+sse3,+sse4.1,+sse4.2 -m 2048 -smp 4
 
